0c76b7a9e3ed302885d236c1ca63afe6e9df0220
Barry Kelly <barry.j.kelly@gmail.com>
2013-10-28 13:42:37 +0000

    Update

---
+++ base/posix/home/.emacs.d/.cask/24.3.1/bootstrap/commander-20131023.1801/commander-autoloads.el
@@ -0,0 +1,18 @@
+;;; commander-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("commander-pkg.el" "commander.el") (21102
+;;;;;;  25137 356863 920000))
+
+;;;***
+
+(provide 'commander-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; commander-autoloads.el ends here
