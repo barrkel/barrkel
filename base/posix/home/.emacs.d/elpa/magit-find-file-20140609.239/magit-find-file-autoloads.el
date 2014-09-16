b51d3e25db8f116dac190a547abee0ffe55abd90
Barry Kelly <barry.j.kelly@gmail.com>
2014-09-16 13:58:44 +0100

    Upgrade elpa

--- base/posix/home/.emacs.d/elpa/magit-find-file-20140609.239/magit-find-file-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; magit-find-file-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (magit-find-file-completing-read) "magit-find-file"
-;;;;;;  "magit-find-file.el" (21435 60771 876569 993000))
-;;; Generated autoloads from magit-find-file.el
-
-(autoload 'magit-get-top-dir "magit")
-
-(autoload 'magit-git-lines "magit")
-
-(autoload 'magit-find-file-completing-read "magit-find-file" "\
-Uses a completing read to open a file from git ls-files
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("magit-find-file-pkg.el") (21435 60771
-;;;;;;  911328 67000))
-
-;;;***
-
-(provide 'magit-find-file-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; magit-find-file-autoloads.el ends here
