9d426d754b9876fc547fad62ad94f03391774959
Barry Kelly <barry.j.kelly@gmail.com>
2015-02-22 09:02:30 +0000

    Upgrade emacs packages

---
+++ base/posix/home/.emacs.d/elpa/frame-fns-20150104.2208/frame-fns-autoloads.el
@@ -0,0 +1,18 @@
+;;; frame-fns-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("frame-fns-pkg.el" "frame-fns.el") (21737
+;;;;;;  39015 332417 655000))
+
+;;;***
+
+(provide 'frame-fns-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; frame-fns-autoloads.el ends here
--- base/posix/home/.emacs.d/elpa/go-projectile-20140826.948/go-projectile-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; go-projectile-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("go-projectile-pkg.el" "go-projectile.el")
-;;;;;;  (21530 41334 980978 811000))
-
-;;;***
-
-(provide 'go-projectile-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; go-projectile-autoloads.el ends here
