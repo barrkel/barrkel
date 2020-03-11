7012271be98a3f58b17adab2a140068f0ef40206
Barry Kelly <barry.j.kelly@gmail.com>
2020-03-11 05:16:50 +0000

    Update emacs again

---
+++ base/posix/home/.emacs.d/elpa/git-commit-20200207.1819/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20200207.1819" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103")) :commit "ef47f0c660c4026d69f06faf3611c2dfd7a2a30b" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
